57c769f4086070dd8f56b33010fbff8573e42c3a
Trystan Semmens <trystansemmens@outlook.com>
2023-03-07 17:25:25 -0500

    separate voice controls

--- main.py
+++ main.py
@@ -31,17 +31,16 @@ if __name__ == "__main__":
         motor.zero()
 
     # set up speech to text AI
-    # ds = deepspeech.Model('SpeechModel/deepspeech-0.9.3-models.pbmm')
+    ds = deepspeech.Model('SpeechModel/deepspeech-0.9.3-models.pbmm')
 
     # start listening
-    # p = pyaudio.PyAudio()
-    # stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=CHUNK)
 
     # Start an infinite loop for the control system
     print('starting input')
     while 1:
         # Interpret Speech
-        '''
         audio = np.fromstring(stream.read(DURATION * RATE), dtype=np.int16)
         wavio.write("SpeechModel/wave.wav", audio, RATE, sampwidth=p.get_sample_size(format=pyaudio.paInt16))
         print('running deepspeech')
@@ -60,33 +59,5 @@ if __name__ == "__main__":
             motor_l.drive_to(instr.angle)
         else:
             pass
-        '''
-
-        # get state of all buttons on controller
-        state = XInput.get_state(0)
-        triggers = XInput.get_trigger_values(state)
-        buttons = XInput.get_button_values(state)
-        sticks = XInput.get_thumb_values(state)
-
-        # set all motors based on controller values
-        conveyor_l.drive((triggers[0] - triggers[1]))
-        conveyor_r.drive((triggers[1] - triggers[0]))
-
-        if buttons['RIGHT_SHOULDER']:
-            scissors.increment(SPEED)
-        elif buttons['LEFT_SHOULDER']:
-            scissors.increment(-SPEED)
-
-        if buttons['DPAD_RIGHT']:
-            wing_r.increment(-SPEED)
-            wing_l.increment(SPEED)
-        elif buttons['DPAD_LEFT']:
-            wing_r.increment(SPEED)
-            wing_l.increment(-SPEED)
-
-        if buttons['A']:
-            for motor in all_motors:
-                motor.zero()
 
         time.sleep(0.001)
-    print('all done')
