cf3b83e580498f4e67a56e2f29f9e2924317ec5e
Trystan Semmens <trystansemmens@outlook.com>
2023-03-09 19:23:48 -0500

    added limits to motors added acceleration profile added motor configs

--- Motor.py
+++ Motor.py
@@ -3,8 +3,13 @@ class Servo:
     MIN_PULSE = 0  # minimum pulse width in ms
     MAX_PULSE = 2
     ANGLE_CONST = 1  # measure the actual range of motion of the motor and get a number for total degrees spun / 180
+    ACC_CURVE = [0]
+    i = 1
+    while i < 100:
+        ACC_CURVE.append((i-1)/i)
+        i += 1
 
-    def __init__(self, pin, board, speed, min_angle=0, max_angle=180):
+    def __init__(self, pin, board, speed, min_angle=0, max_angle=180, zero_pos=90):
         board.set_pin_mode_servo(pin, self.MIN_PULSE, self.MAX_PULSE)
         self.board = board
         self.pin = pin
@@ -12,6 +17,8 @@ class Servo:
         self.speed = speed
         self.min_angle = min_angle
         self.max_angle = max_angle
+        self.acc_curve_pos = 0
+        self.zero_pos = zero_pos
 
     def drive_to(self, angle):
         if self.min_angle <= angle <= self.max_angle:
@@ -19,16 +26,23 @@ class Servo:
             self.angle = angle
 
     def drive(self, control_input=1):
+        if control_input == 0:
+            self.acc_curve_pos = 0
+        elif self.acc_curve_pos < 99:
+            self.acc_curve_pos += 1
         if self.min_angle < self.angle + self.speed * control_input < self.max_angle:
-            self.angle += self.speed * control_input
+            self.angle += self.speed * control_input * self.ACC_CURVE[self.acc_curve_pos]
         self.drive_to(self.angle)
 
     def drive_continuous(self, control_input):
         self.drive_to(90 + 90 * control_input)
 
-
     def zero(self):
-        self.drive_to(90)
+        self.drive_to(self.zero_pos)
+
+    def set_zero(self):
+        self.zero_pos = self.angle
+        return self.zero_pos
 
 
 class Motor:
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import time
 from pymata4 import pymata4 as arduino
 import XInput
+import json
 from Motor import Servo, CServo
 
+CONFIG_DATA = 'motor_configs'
+
 if __name__ == "__main__":
+    # load config file
+    file = open(CONFIG_DATA)
+    saved_configs = json.load(file)
+    file.close()
     # set up the electrical
     board = arduino.Pymata4()
     conveyor_l = Servo(13, board, 2)
     conveyor_r = Servo(12, board, 2)
-    scissors = Servo(11, board, 1)
-    wing_l = Servo(10, board, 1)
-    wing_r = Servo(9, board, 1)
+    scissors = Servo(11, board, 1, min_angle=86, max_angle=136, zero_pos=saved_configs['11'])
+    wing_l = Servo(10, board, 2, min_angle=64, max_angle=180, zero_pos=saved_configs['10'])
+    wing_r = Servo(9, board, 2, min_angle=59, max_angle=180, zero_pos=saved_configs['9'])
     all_motors = [conveyor_l, conveyor_r, scissors, wing_l, wing_r]
 
     # initialize the hardware
@@ -50,6 +57,19 @@ if __name__ == "__main__":
         if buttons['A']:
             for motor in all_motors:
                 motor.zero()
+
+        if buttons['B']:
+            save_data = dict()
+            for motor in all_motors:
+                motor.set_zero()
+                save_data[str(motor.pin)] = motor.zero_pos
+            file = open(CONFIG_DATA, 'w+')
+            json.dump(save_data, file)
+            file.close()
+
+        if buttons['DPAD_DOWN']:
+            for motor in all_motors:
+                print(motor.angle)
 
 
         time.sleep(0.01)
